3a880b1f817bb7f75ea1471c49d66ce504b5c87d
13010-18-11 <13010-18-11@exchange.ad.afpanet>
2018-08-30 10:04:12 +0200

    =creation vieyx plateux de jeux initila

---
+++ src/AppBundle/Model/Board.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace AppBundle\Model;
+
+class Board {
+
+    const BOARD = array(
+        0 => Array('index' => 20, 'valeur' => 100),
+        1 => Array('index' => 21, 'valeur' => 0),
+        2 => Array('index' => 22, 'valeur' => 50),
+        3 => Array('index' => 23, 'valeur' => 100),
+        4 => Array('index' => 24, 'valeur' => 0),
+        5 => Array('index' => 25, 'valeur' => 100),
+        6 => Array('index' => 26, 'valeur' => 0),
+        7 => Array('index' => 27, 'valeur' => 5),
+        8 => Array('index' => 28, 'valeur' => 1),
+        9 => Array('index' => 29, 'valeur' => 100),
+        10 => Array('index' => 30, 'valeur' => 5),
+        11 => Array('index' => 19, 'valeur' => 50),
+        12 => Array('index' => 31, 'valeur' => 100),
+        13 => Array('index' => 18, 'valeur' => 0),
+        14 => Array('index' => 32, 'valeur' => 0),
+        15 => Array('index' => 17, 'valeur' => 1),
+        16 => Array('index' => 33, 'valeur' => 0),
+        17 => Array('index' => 16, 'valeur' => 100),
+        18 => Array('index' => 34, 'valeur' => 50),
+        19 => Array('index' => 15, 'valeur' => 100),
+        20 => Array('index' => 35, 'valeur' => 0),
+        21 => Array('index' => 14, 'valeur' => 50),
+        22 => Array('index' => 36, 'valeur' => 50),
+        23 => Array('index' => 13, 'valeur' => 5),
+        24 => Array('index' => 37, 'valeur' => 100),
+        25 => Array('index' => 12, 'valeur' => 100),
+        26 => Array('index' => 38, 'valeur' => 0),
+        27 => Array('index' => 11, 'valeur' => 0),
+        28 => Array('index' => 39, 'valeur' => 1),
+        29 => Array('index' => 10, 'valeur' => 0),
+        30 => Array('index' => 9, 'valeur' => 100),
+        31 => Array('index' => 8, 'valeur' => 100),
+        32 => Array('index' => 7, 'valeur' => 5),
+        33 => Array('index' => 6, 'valeur' => 5),
+        34 => Array('index' => 5, 'valeur' => 5),
+        35 => Array('index' => 4, 'valeur' => 100),
+        36 => Array('index' => 3, 'valeur' => 1),
+        37 => Array('index' => 2, 'valeur' => 50),
+        38 => Array('index' => 1, 'valeur' => 1),
+        39 => Array('index' => 33, 'valeur' => 50),
+        40 => Array('index' => 0, 'valeur' => 50),
+    );
+
+}
